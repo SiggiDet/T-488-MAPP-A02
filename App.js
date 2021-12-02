568a8a9cdc8ef1ebe634d92134cf0068bf584dc9
Sigurður Detlef <siggidetjons6@gmail.com>
2021-12-02 10:48:24 +0000

    Implemented Search bar but needs proper work

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import SearchBar from './src/SearchBar';
 
 const imageDirectory = `${FileSystem.documentDirectory}images`;
@@ -88,8 +89,8 @@ function MyStack() {
 export default function App() {
   return (
     <NavigationContainer>
+      <MyStack />
       <SearchBar/>
-      {/* <MyStack /> */}
     </NavigationContainer>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,27 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput } from "react-native";
-
+import { View, TextInput, StyleSheet, Keyboard, Button  } from "react-native";
+import { Feather, Entypo } from "@expo/vector-icons";
 
 const SearchBar = (props) => {
+    const [ContactNames, setInputName] = useState('');
+    const [PhoneNrs, setPhotoURI] = useState('');
+
     return(
-        <View>
-            <TextInput style = {styles.SearchBar.input}>
-                
-            </TextInput>
+        <View style={styles.container}>
+
+        <View style={props.clicked, styles.searchBarNotClicked, styles.searchBarIsClicked}>
+
+          {/* search Icon */}
+          <Feather name="search" size={20} color="black" style={styles.SearchIconLocation}/>
+
+          {/* Input field */}
+          <TextInput style={styles.input} placeholder="Search Contact" value={props.searchPhrase} 
+          onChangeText={props.setSearchPhrase} onFocus={() => {props.setClicked(true);}}/>
+
+          {/* cross Icon, depending on whether the search bar is clicked or not */}
+          {props.clicked && 
+          (<Entypo name="cross" size={20} color="black" style={{ padding: 1 }} onPress={() => {props.setSearchPhrase("")}}/>)}
         </View>
+
+        {/* cancel button, depending on whether the search bar is clicked or not */}
+        {props.clicked && (
+        <View>
+            <Button title="Cancel" onPress={() => {Keyboard.dismiss(); props.setClicked(false);}} />
+        </View>)}
+
+      </View>
     )
 }
 
 
 const styles = StyleSheet.create({
     container:{
-      margin: 15,
+      margin: 5,
       justifyContent: "flex-start",
       alignItems: "center",
       flexDirection: "row",
       width: "90%",
     },
-    searchBar__clicked:{
+    SearchIconLocation:{
+        marginLeft: 1
+    },
+    searchBarNotClicked: {
+        padding: 10,
+        flexDirection: "row",
+        width: "95%",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+      },
+    searchBarIsClicked:{
         padding: 10,
         flexDirection: "row",
         width: "80%",
