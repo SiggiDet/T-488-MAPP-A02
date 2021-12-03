0061e370b33a13c52e61981d93e0f251621c2df7
KyplerLloyd <kypler20@ru.is>
2021-12-03 20:18:03 +0000

    modify operation needed

--- src/services/detailsContacts.js
+++ src/services/detailsContacts.js
@@ -1,65 +1,62 @@
 import React from 'react';
-import { Button, Image, View, Text, TouchableOpacity, Linking, StyleSheet} from 'react-native';
+import { Image, View, Text, TouchableOpacity, Linking, StyleSheet} from 'react-native';
 
 
 const ContactDetail = ( props ) => {
 
-    console.log("HELLO")
-    console.log(props.route.params.data)
-	return (
-		<View style={{ flex: 1, alignItems: 'center'}}>
-			<Image style={styles.avatar} source={ {uri: props.route.params.data.imageURI} } />
-			<Text style={styles.name}> {props.route.params.data.name} </Text>
-            <Text style={styles.name}> {props.route.params.data.phone} </Text>
-			<TouchableOpacity
-				onPress={() => { Linking.openURL(`tel:${"555-7777"}`); }}>
-                <Button title="Call" variant="success">Call</Button>
-			</TouchableOpacity>
-		</View>
-	);
+    return (
+        <View style={styles.body}>
+            <Image style={styles.profile} source={ {uri: props.route.params.data.imageURI} } />
+            <Text style={styles.name}> {props.route.params.data.name} </Text>
+            <Text style={styles.number}> {props.route.params.data.phone} </Text>
+            <TouchableOpacity>
+                <Text onPress={()=>{Linking.openURL(`tel:${props.route.params.data.phone}`);}} style={styles.button}>Call</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 export default ContactDetail;
 
 const styles = StyleSheet.create({
-	header: {
-		backgroundColor: "white",
-		height: 200
-	},
-	avatar: {
-		width: 130,
-		height: 130,
-		borderRadius: 63,
-		borderWidth: 4,
-		borderColor: "white",
-		marginBottom: 10,
-		alignSelf: 'center',
-		marginTop: 130
-	},
-	body: {
-		flex: 1,
-		marginTop: 60
-	},
-	bodyContent: {
-		flex: 1,
-		alignItems: 'center',
-		padding: 30,
-		paddingBottom: 20,
-		borderWidth: 1,
-		borderColor: 'gray'
-	},
-	name: {
-		fontSize: 35,
-		color: "#696969",
-		fontWeight: "600",
-		alignSelf: 'center',
-		justifyContent: 'center'
-	},
-	info: {
-		fontSize: 20,
-		color: "gray",
-		marginTop: 40,
-		borderWidth: 1,
-		borderColor: 'gray'
-	},
+
+    profile: {
+        width: 130,
+        height: 130,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: "#696969",
+        marginBottom: 40,
+        alignSelf: 'center',
+        marginTop: 70
+    },
+    body: {
+        flex: 1,
+        marginTop: 10,
+        alignItems: 'center'
+    },
+    name: {
+        fontSize: 60,
+        color: "#696969",
+        fontWeight: "600",
+        alignSelf: 'center',
+    },
+    number: {
+        fontSize: 30,
+        color: "#696969",
+        fontWeight: "600",
+        alignSelf: 'center',
+    },
+    button: {
+        alignItems: "center",
+        marginTop: 40,
+        fontSize: 30,
+        height: 32,
+        width: 60,
+        alignSelf: 'center',
+        backgroundColor: "#4CAF50",
+        color: "#ffffff",
+        textAlign: "center",
+        fontWeight: "500",
+    }
 });
